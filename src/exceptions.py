d71736bb19836c744d4941c2df5aeba1b73f47c6
Devansh Sengar <devanshsengar8642@gmail.com>
2024-07-05 15:57:23 +0530

    fix type annotations in exceptions

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,11 +1,16 @@
-from enum import Enum
+from enum import IntEnum
+from typing import TypedDict
 
-class ExceptionCode(Enum):
+class ExceptionCode(IntEnum):
     DISCONNECT = 0
     INVALID_HEADER = 1
     BAD_REQUEST = 2
     NOT_FOUND = 4
     USER_EXISTS = 5
+
+class RequestExceptionDict(TypedDict):
+    msg: str
+    code: int
 
 class RequestException(Exception):
     def __init__(self, msg: str, code: ExceptionCode, *args: object) -> None:
@@ -14,9 +19,9 @@ class RequestException(Exception):
         self.code = code
 
     @classmethod
-    def to_dict(cls, exception) -> dict[str, str | int]:
+    def to_dict(cls, exception: "RequestException") -> RequestExceptionDict:
         return {"msg": exception.msg, "code": exception.code.value}
 
     @classmethod
-    def from_dict(cls, data: dict) -> "RequestException":
+    def from_dict(cls, data: RequestExceptionDict) -> "RequestException":
         return cls(msg=data["msg"], code=ExceptionCode(data["code"]))
